73a8aac2269b0b9c1188039f4cd62f3a060ba744
anupamme <mediratta@gmail.com>
2026-08-23 22:20:36 +0000

    fix: gitlab.bandit.B313.B314.B315.B316.B318.B319.B320.B405.B406.B407.B408.B409.B410 security vulnerability
    Automated security fix generated by OrbisAI Security

--- moveit2_server/moveit2_server/context.py
+++ moveit2_server/moveit2_server/context.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from math import isfinite
 import posixpath
-import xml.etree.ElementTree as ET
+import defusedxml.ElementTree as ET
 from pathlib import Path
 from typing import Any
 
